92c1c080788db168d3b757fa1bdb032c5f3b38a7
captaincoro11 <captaincoro444@gmail.com>
2024-05-26 15:52:19 +0530

    comimit

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const app = express();
+const dotenv = require('dotenv');
+const { connectDatabase } = require('./db');
+const taskRoutes = require('./routes/task');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
-dotenv.config();
 
-const port = process.env.PORT || 4000;
+dotenv.config({});
+
+connectDatabase();
+app.use(cors());
+app.use(express.json({limit:"50mb"}))
+app.use(express.static('public'))
+app.use('/task',taskRoutes)
+
+
+const port = process.env.port || 5000;
 
 app.get('/',(req,res)=>{
-    res.send("Hi therr")
-});
+    res.json("HELLO BABY")
+})
+
+
+
 
 app.listen(port,()=>{
-    console.log("Server stareted on port")
+    console.log(`It started on ${port}`)
 })
